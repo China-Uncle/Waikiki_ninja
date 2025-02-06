6bd1fbc3756ea7437b6ea43effb82339e92b3360
马艳龙 <myl86898244@gmail.com>
2025-02-06 13:23:17 +0800

    更改获取token

--- backend/ql.js
+++ backend/ql.js
@@ -4,18 +4,30 @@ const got = require('got');
 require('dotenv').config();
 const { readFile } = require('fs/promises');
 const path = require('path');
-
-const qlDir = process.env.QL_DIR || '/ql';
-const authFile = path.join(qlDir, 'config/auth.json');
+ 
+const client_id=process.env.CLIENT_ID;
+const client_secret=process.env.CLIENT_SECRET;
 
 const api = got.extend({
-  prefixUrl: process.env.QL_URL || 'http://localhost:5600',
+  prefixUrl: process.env.QL_URL || 'http://localhost:5700',
   retry: { limit: 0 },
 });
 
 async function getToken() {
-  const authConfig = JSON.parse(await readFile(authFile));
-  return authConfig.token;
+  const body = await api({
+    url: 'api/envs',
+    searchParams: {
+      client_id: client_id,
+      client_secret: client_secret,
+      t: Date.now(),
+    },
+    headers: {
+      Accept: 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+  }).json();
+ 
+  return body.data.token;
 }
 
 module.exports.getEnvs = async () => {
